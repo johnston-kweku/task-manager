3da08b73b7f7a455ce3c50e227e6eaf833b17a6c
Johnston Abubakar Kweku <143568779+johnston-kweku@users.noreply.github.com>
2026-04-08 18:48:42 +0000

    Add

--- main_app/models.py
+++ main_app/models.py
@@ -3,10 +3,21 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
+    class PriorityLevel(models.TextChoices):
+        LEVEL_1 = '1', '1'
+        LEVEL_2 = '2','2'
+        LEVEL_3 = '3', '3'
+        
     name = models.CharField(max_length=100, blank=False)
     description = models.TextField(max_length=1000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     duration = models.DurationField()
     completed = models.BooleanField(default=False)
+    priority_level = models.CharField(
+        max_length=10,
+        choices=PriorityLevel.choices,
+        default=PriorityLevel.LEVEL_1
+    )
 
-    
+
+
