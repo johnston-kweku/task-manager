96654a6c5d6259a131b82d07683d60196b21c77d
Johnston Abubakar Kweku <143568779+johnston-kweku@users.noreply.github.com>
2026-04-08 10:16:51 +0000

    Add alpine.js

--- static/js/index.js
+++ static/js/index.js
@@ -2,4 +2,14 @@ import { Alpine } from "alpinejs";
 
 window.Alpine = Alpine;
 
+
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('user', () => ({
+        count: 10,
+        name: 'Johnston',
+        search: ''
+    }))
+})
+
 Alpine.start()
