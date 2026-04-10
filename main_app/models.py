54ccef9636c3ce7e02b7b3c07d2a45b1d37b800f
Johnston Abubakar Kweku <143568779+johnston-kweku@users.noreply.github.com>
2026-04-10 22:19:25 +0000

    Do good

--- main_app/models.py
+++ main_app/models.py
@@ -4,19 +4,18 @@ from django.db import models
 
 class Task(models.Model):
     class PriorityLevel(models.TextChoices):
-        LEVEL_1 = '1', '1'
-        LEVEL_2 = '2','2'
-        LEVEL_3 = '3', '3'
-        
+        LEVEL_1 = 'THREAT_LEVEL_RED', 'THREAT_LEVEL_RED'
+        LEVEL_2 = 'NODE_VALIDATION','NODE_VALIDATION'
+        LEVEL_3 = 'GHOST_PROTOCOL', 'GHOST_PROTOCOL'
     name = models.CharField(max_length=100, blank=False)
     description = models.TextField(max_length=1000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     duration = models.DurationField()
     completed = models.BooleanField(default=False)
     priority_level = models.CharField(
-        max_length=10,
+        max_length=100,
         choices=PriorityLevel.choices,
-        default=PriorityLevel.LEVEL_1
+        default=PriorityLevel.LEVEL_3
     )
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -4,5 +4,21 @@ from .models import Task
 # Create your views here.
 
 def home(request):
-    tasks = Task.objects.filter(completed=False).count()
+    tasks = Task.objects.filter(completed=False)
     return render(request, 'main_app/index.html', {'tasks':tasks})
+
+
+
+def command_center(request):
+    # Context data for your charts (we'll make this real later)
+    context = {
+        'tasks_completed': 12,
+        'uptime': "10:04:22",
+    }
+    
+    # Check if the request header says it's from our AJAX call
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return render(request, 'main_app/partials/command_center.html', context)
+    
+    # Otherwise, return the full page wrapper
+    return render(request, 'main_app/index.html', context)
